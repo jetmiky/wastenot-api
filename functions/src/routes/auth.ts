9858c59e4e26cea82d58648e865ba8f776d812fe
Bima Jatmiko Abadi <bja.devs@gmail.com>
2024-03-23 13:59:12 +0700

    Add edit profile routes

--- functions/src/routes/auth.ts
+++ functions/src/routes/auth.ts
@@ -3,6 +3,7 @@ import Router = require("@koa/router");
 import Joi = require("joi");
 
 import { getAsyncErrorMessage } from "../utils/errors";
+import verifyToken from "../middlewares/tokens";
 
 const router = new Router();
 
@@ -12,22 +13,63 @@ router.post("/register", async (ctx) => {
     email: Joi.string().email().required(),
     password: Joi.string().pattern(/^[a-zA-Z0-9]{8,30}/, {}),
     confirm_password: Joi.ref("password"),
+    phoneNumber: Joi.string()
+      .pattern(/^\\+62\\d{10,12}$/)
+      .required(),
   }).with("password", "confirm_password");
 
   try {
-    const { name, email, password } = await schema.validateAsync(ctx.req.body);
+    const { name, email, password, phoneNumber } = await schema.validateAsync(
+      ctx.req.body
+    );
 
     const user = await admin.auth().createUser({
       displayName: name,
       email,
       password,
+      phoneNumber,
       emailVerified: false,
       disabled: false,
     });
 
     await admin.auth().setCustomUserClaims(user.uid, { role: "user" });
 
-    return ctx.ok({ uid: user.uid, email: user.email });
+    return ctx.created({ uid: user.uid, email: user.email });
+  } catch (error) {
+    const message = getAsyncErrorMessage(error);
+
+    if (error instanceof Joi.ValidationError) {
+      return ctx.badRequest(message);
+    }
+
+    return ctx.internalServerError(message);
+  }
+});
+
+router.put("/", verifyToken("user"), async (ctx) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().pattern(/^[a-zA-Z0-9]{8,30}/, {}),
+    confirm_password: Joi.ref("password"),
+    phoneNumber: Joi.string()
+      .pattern(/^\\+62\\d{10,12}$/)
+      .required(),
+  }).with("password", "confirm_password");
+
+  try {
+    const { name, email, password, phoneNumber } = await schema.validateAsync(
+      ctx.req.body
+    );
+
+    await admin.auth().updateUser(ctx.state.uid, {
+      displayName: name,
+      email,
+      password,
+      phoneNumber,
+    });
+
+    return ctx.ok({ uid: ctx.state.uid, email });
   } catch (error) {
     const message = getAsyncErrorMessage(error);
 
