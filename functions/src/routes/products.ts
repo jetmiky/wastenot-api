83cef4b288f725e95d20980b628aacfba555f657
Bima Jatmiko Abadi <bja.devs@gmail.com>
2024-03-24 20:47:25 +0700

    Add route to modify product

--- functions/src/routes/products.ts
+++ functions/src/routes/products.ts
@@ -3,7 +3,7 @@ import Joi = require("joi");
 
 import verifyToken from "../middlewares/tokens";
 import db from "../utils/db";
-import { NotFoundError } from "../types/Error";
+import { ForbiddenError, NotFoundError } from "../types/Error";
 import Product from "../types/Product";
 
 const router = new Router();
@@ -63,6 +63,50 @@ router.post("/", verifyToken("bank"), async (ctx) => {
 
   const { id } = await db.products.add(product);
   ctx.created({ id, ...product });
+});
+
+router.put("/:id", verifyToken("bank"), async (ctx) => {
+  const productId = ctx.params.id;
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    description: Joi.string().min(3).max(254).required(),
+    price: Joi.number().min(0).required(),
+    marketplaces: Joi.array()
+      .min(1)
+      .items({
+        name: Joi.string().required(),
+        url: Joi.string().required(),
+      })
+      .required(),
+  });
+
+  const body = await schema.validateAsync(ctx.request.body);
+  const { name, description, price, marketplaces } = body;
+
+  const document = await db.products.doc(productId).get();
+
+  if (!document.exists) throw new NotFoundError();
+
+  const product = document.data() as Product;
+  if (product.ownerId !== ctx.state.uid) throw new ForbiddenError();
+
+  const updatedProduct = { ...product, name, description, price, marketplaces };
+  await db.products.doc(productId).set(updatedProduct);
+
+  ctx.ok(updatedProduct);
+});
+
+router.delete("/:id", verifyToken("bank"), async (ctx) => {
+  const productId = ctx.params.id;
+
+  const document = await db.products.doc(productId).get();
+  if (!document.exists) throw new NotFoundError();
+
+  if (document.data()?.ownerId !== ctx.state.uid) throw new ForbiddenError();
+  await db.products.doc(productId).delete();
+
+  ctx.noContent();
 });
 
 export default router;
