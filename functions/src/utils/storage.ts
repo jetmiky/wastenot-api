15616c4616fa3a82f3f39fced43cfb4e25423eb4
Bima Jatmiko Abadi <bja.devs@gmail.com>
2024-03-26 21:02:16 +0700

    Implement public and private files in storage

--- functions/src/utils/storage.ts
+++ functions/src/utils/storage.ts
@@ -9,35 +9,35 @@ type DirectoryPath = "badges" | "delivers" | "pickups" | "products" | "misc";
  * @param { string } filename     Desired name of file uploaded, includes ext.
  * @param { string } contentType  Content type of file, ie. image/png.
  * @param { Buffer } data         Buffer data of file.
+ * @param { boolean } makePublic  Make the file accessible by public.
  * @return { Promise<string> }    Returns public URL of uploaded file.
  */
 export async function upload(
   path: DirectoryPath,
   filename: string,
   contentType: string,
-  data: Buffer
+  data: Buffer,
+  makePublic?: boolean
 ): Promise<string> {
-  const bucket = getStorage().bucket().file(`${path}/${filename}`);
-  await bucket.save(data, { contentType });
+  const file = getStorage().bucket().file(`${path}/${filename}`);
+  await file.save(data, { contentType });
 
-  return bucket.publicUrl();
+  if (makePublic) await file.makePublic();
+
+  return file.publicUrl();
 }
 
 /**
- * Get public URL of file in Storage.
+ * Get signed URL of file in Storage.
  *
- * @param { DirectoryPath } path         Path relative to root storage.
- * @param { string } filename     Name of file, includes ext.
- * @return { Promise<string> }    Returns public URL of file.
+ * @param { string } filepath Relative path of file to root, includes ext.
+ * @return { Promise<string> } Returns signed URL of file.
  */
-export async function getSignedUrl(
-  path: DirectoryPath,
-  filename: string
-): Promise<string> {
+export async function getSignedUrl(filepath: string): Promise<string> {
   const expires = new Date();
   expires.setDate(expires.getDate() + 7);
 
-  const bucket = getStorage().bucket().file(`${path}/${filename}`);
+  const bucket = getStorage().bucket().file(filepath);
   const response = await bucket.getSignedUrl({ action: "read", expires });
 
   return response[0];
